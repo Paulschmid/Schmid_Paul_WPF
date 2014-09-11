6b8cad702c515f167ab69309bc7c3bc636966a95
Paulschmid <paulschmid@Pauls-MacBook-Pro.local>
2014-09-11 01:53:54 -0500

    vehicle

--- Industry-Specific/js/script.js
+++ Industry-Specific/js/script.js
@@ -2,4 +2,13 @@
 //09/10//14
 //Industry-Specific
 
+//How many quarts of oil will you use?
+
+vehicle=prompt("What type of vehicle do you own?");
+console.log(vehicle);
+
+alert(" Now lets see how many quarts your "+vehicle+" should use!");
+
+
+
 
