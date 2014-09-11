65c045756200910c0c4e675a4693305afaa3f149
Paulschmid <paulschmid@Pauls-MacBook-Pro.local>
2014-09-11 11:53:46 -0500

    added name
    added name prompt

--- Wacky/js/script.js
+++ Wacky/js/script.js
@@ -1,3 +1,15 @@
-/**
- * Created by paulschmid on 9/11/14.
- */
+//paul schmid
+// 9/11/2014/
+// Wacky Weight**
+
+ var first=prompt ("What is your first name?");
+console.log(first);
+
+var last=prompt ("What is your last name?");
+console.log(last);
+
+
+alert(" Welcome "+first+last+"! I would like to help you with your weight! What weight will you be plus or minus over time?");
+
+var age=prompt(" How old are you ");
+console.log(age);
