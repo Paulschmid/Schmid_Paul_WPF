708370e0c1083bee0a98393d70e7843c10727763
Paulschmid <paulschmid@Pauls-MacBook-Pro.local>
2014-09-18 12:44:39 -0500

    made conditionals wacky
    made started conditionals wacky , include prompts, alerts, and
    validated conditionals

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -2,9 +2,64 @@
 //9/16/2014
 //conditionals
 
-alert("Welcome! We are showing 3 movies this weekend.")
-alert("Lets get some info to see what movies you can see!")
 
-personAge=prompt("How old are you?")//Age of child
-//if less than 18, print to console "Parent Required"
-parentAge=prompt("What is the age of parent or adult present? ")//age of parent or adult
+alert(" This movie requires that all viewers be over the age of 17 or accompanied by an adult.");
+
+    var kidAge  = prompt("How old are you?");
+      console.log(kidAge);
+
+
+
+
+    var minAge= 17;
+    var childAge = kidAge;
+    var adultAge = oldTimer;
+    var noInput = "";
+
+//if child is over the age of 17; they are eligible for entry.
+if (kidAge == noInput)  {
+    var noInput = prompt(" Make sure to input your age?");
+    console.log(noInput);
+}else if
+        (childAge >= minAge){
+            //code in position is true
+                     alert( "Enjoy the movie!");
+                         console.log("Enjoy the movie!");
+}
+        else {
+    alert(" You will need an adult to view this movie");
+    console.log(" You will need an adult to see this movie ");
+    //performed if condition is false
+    //console.log("You will need an adult in order to see this movie");
+
+
+    var oldTimer = adultAge;
+
+    var oldTimer = prompt(" What age is the adult present?");
+    console.log(oldTimer);
+    if (oldTimer == noInput) {
+        var noInput = prompt("Make sure to input adults age!");
+        console.log(noInput);
+    } else if
+        (oldTimer <= minAge);
+
+     else {
+        //child and adult are allowed to see the movie!
+        alert("With the company of this adult! You are allowed to view the movie!");
+        console.log("Adult and child are allowed to see the movie!");
+    }
+}
+
+
+
+
+
+//function required == "" {
+    //var childAge = "kidAge";
+ //   if (childAge == "") {
+      //  alert("Please input a Value")
+    //    return false;
+
+   // else {
+  //      alert('Code has been accept');
+   // }
