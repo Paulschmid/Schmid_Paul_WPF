3651dbc5092b674140490e7a6e8d2fd852419a5b
Paulschmid <paulschmid@Pauls-MacBook-Pro.local>
2014-09-18 22:37:46 -0500

    variables
    added var for age

--- conditionals_game/js/script.js
+++ conditionals_game/js/script.js
@@ -7,3 +7,9 @@ alert(" This game requires 18 yrs or older to buy!");
 
 var ageA  = prompt("How old is purchaser");
 console.log(ageA);
+
+var minAge= 18;
+var adultMin=18;
+var childAge = kidAge;
+var adultAge = oldTimer;
+var noInput = "";
