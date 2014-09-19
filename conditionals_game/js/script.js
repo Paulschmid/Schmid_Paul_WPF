6cb6f99d28c0c8c0103ead95acb320fd9c95899f
Paulschmid <paulschmid@Pauls-MacBook-Pro.local>
2014-09-18 23:02:27 -0500

    making sure
    making sure conditional game is fully added

--- conditionals_game/js/script.js
+++ conditionals_game/js/script.js
@@ -9,7 +9,7 @@ var ageA  = prompt("How old is purchaser");
 console.log(ageA);
 
 var ageMin = 18;
-var adultA=18;
+var adultMin=18;
 var ageB = ageA;
 var adultA = adultB;
 var noInput = "";
@@ -19,7 +19,7 @@ if (ageA == noInput)  {
     var noInput = prompt("No age was added! Age must be imputed to continue!");
     console.log(noInput);
 }else if
-    (ageA >= ageMin){
+    (ageB >= ageMin){
     //If age of purchaser is older than or equal to 18 they can purchase
     alert( "You are of age to purchase this game!! Congrats");
     console.log("Able to purchase game");
@@ -29,3 +29,28 @@ else {
     alert(" You are not over 18! To purchase get an adult!");
     console.log(" To purchase find an parent or guardian ");
 }
+//Asking adult for age
+ var adultA = adultB;
+
+var adultB = prompt("What is the age of adult purchaser");
+console.log(adultB);
+//prompt if no input is added to age!
+if (adultA == noInput) {
+    var noInput = prompt("Adults age not inserted redo age!");
+    console.log(noInput);
+} else if
+    (adultB <= adultMin);
+
+else {
+    //adult must purchase game
+    alert("The adult may purchase the game");
+    console.log("Adult may purchase the game");
+}
+//alert to notify that adult must be over the age of 18
+if (adultB < adultMin) {
+    alert( "Purchaser must be over age 18");
+}
+
+var good= ( ageA>ageMin && adultB>adultMin);
+console.log(good);
+
