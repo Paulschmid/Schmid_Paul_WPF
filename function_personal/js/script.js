136f2d5f41c023d38d3d59032269c7ab5707fb68
Paulschmid <paulschmid@Pauls-MacBook-Pro.local>
2014-09-23 11:19:17 -0500

    console.log
    added console .log on all functions

--- function_personal/js/script.js
+++ function_personal/js/script.js
@@ -6,8 +6,10 @@ alert("lets find your bmi ")
 var a =12
 var feet=prompt('First lets see how tall you are! Please input you height.(Ft only)');
 console.log(feet);
+console.log("------height ft------");
 var inches=prompt('Now that we have the ft, please input your inches');
 console.log(inches);
+console.log("------height inches------");
 
 
 var calcInches = function( a,feet,inches) {//defining
@@ -19,19 +21,21 @@ var calcInches = function( a,feet,inches) {//defining
 }
 var total = calcInches(feet, a, inches);//invoking
 console.log(total);
-
+console.log("---total height inches---");
 alert("you are "+total+" inches ");
 
  squaredA (total, total);
 function squaredA (a, b) {
     var squared = a * b;
     console.log(squared);
+    console.log("---squared height---");
 }
     var weight = prompt(" Now that we have your height we need your weight.Please input your weight here");
     console.log(weight);
+    console.log("---prompt for weight---");
 
 alert("Calculating your  BMI at "+total+"inches and "+weight+"lbs!");
-
+squared
 
 
 
